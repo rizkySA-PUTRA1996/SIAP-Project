d79628b195258fe2ebefb9119f924a6ec080a2bd
radityanatha <radityanatha20@gmail.com>
2025-06-13 09:57:41 +0800

    update form login

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,16 @@ use App\Http\Controllers\Petugas\StokObatController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('login');
 });
 
-    Route::get('login', [LoginController::class, 'index'])->name('login');
-    Route::post('login', [LoginController::class, 'store']);
-    Route::get('logout', [LoginController::class, 'destroy'])->name('logout');
+Route::get('login', [LoginController::class, 'index'])->name('login');
+Route::post('login', [LoginController::class, 'store']);
+Route::get('logout', [LoginController::class, 'destroy'])->name('logout');
 
-    Route::get('petugas/antrian', [AntrianController::class, 'index'])->name('petugas.antrian');
-    Route::get('petugas/obat', [StokObatController::class, 'index'])->name('petugas.stokObat');
-    Route::get('petugas/riwayat', [RiwayatController::class, 'index'])->name('petugas.riwayatAntrian');
-    Route::get('petugas/detail/{id_resep}', [AntrianDetailController::class, 'show'])->name('petugas.antrianDetail');
+Route::get('petugas/antrian', [AntrianController::class, 'index'])->name('petugas.antrian');
+Route::get('petugas/obat', [StokObatController::class, 'index'])->name('petugas.stokObat');
+Route::get('petugas/riwayat', [RiwayatController::class, 'index'])->name('petugas.riwayatAntrian');
+Route::get('petugas/detail/{id_resep}', [AntrianDetailController::class, 'show'])->name('petugas.antrianDetail');
 
-    Route::resource('admin/obat', ObatController::class)->names('admin.stokObat');
+Route::resource('admin/obat', ObatController::class)->names('admin.stokObat');
